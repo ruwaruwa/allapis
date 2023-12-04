57d45d77c018291833ff17d6f107bad84c7300ed
ruwaruwa <almozali287@gmail.com>
2023-12-04 09:06:04 +0300

    first commit

---
+++ ma.js
@@ -0,0 +1,17 @@
+// 5 canva
+// var canvas = document.getElementById('myCanvas');
+// var context =canvas .get
+// context.fillStyle = 'red';
+// context.fillRect(50, 50, 100, 100);
+
+
+//dom
+
+// // Creating a new paragraph element
+// const newParagraph = document.createElement('myElement');
+
+// // Adding text content to the paragraph
+// newParagraph.textContent = 'This is a new paragraph this!';
+
+// // Appending the paragraph to the document body
+// document.body.appendChild(newParagraph);
